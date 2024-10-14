aab5c0f50d4ac90faa631c38862fe3c9523cf92c
safaasamir <safaasamirsherif@gmail.com>
2024-10-14 19:27:02 +0300

    fix:request api

--- src/services/apiRequests/registrationApiRequest.js
+++ src/services/apiRequests/registrationApiRequest.js
@@ -1,7 +1,10 @@
 import apiInstance from "../interceptor/axiosInstance";
 
 const createRegistrationRequest = async (graduateData) => {
-  const response = await apiInstance.post("registrationRequests", graduateData);
+  const response = await apiInstance.post(
+    "registration-requests",
+    graduateData
+  );
   return response;
 };
 
