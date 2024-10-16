05b053e51509a75b1c4723b0d953869a508b6cdf
safaasamir <safaasamirsherif@gmail.com>
2024-10-16 19:34:22 +0300

    fix: change some Yup validation rules

--- utils/validations/graduationSchema.js
+++ utils/validations/graduationSchema.js
@@ -1,32 +1,80 @@
 import * as Yup from "yup";
 
+const nameRegex = /^[a-zA-Z\s]+$/;
+const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+const mobileRegex = /^(010|011|012|015)\d{8}$/;
+const cityRegex = /^[A-Za-z][a-zA-Z\s]*$/;
+const facultyUniversityRegex = /^[a-zA-Z\s]+$/;
+const courseRegex = /^[a-zA-Z\s_-]*$/;
+
+const createEmploymentTest = (fieldName) => {
+  return Yup.string()
+    .matches(
+      /^[a-zA-Z\s_-]*$/,
+      `${fieldName} must contain only English letters.`
+    )
+    .test("is-employed", `${fieldName} is required.`, function (value) {
+      return this.parent.isEmployed ? !!value : true;
+    });
+};
+
+const stringRequiredWithPattern = (
+  pattern,
+  invalidMessage,
+  requiredMessage
+) => {
+  return Yup.string()
+    .matches(pattern, invalidMessage)
+    .required(requiredMessage);
+};
+
 const stepValidationSchemas = [
   Yup.object().shape({
-    fullName: Yup.string().required("Full Name is required."),
-    personalPhoto: Yup.string().nullable().optional(),
-    email: Yup.string()
-      .matches(
-        /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/,
-        "Please enter a valid email address."
-      )
-      .required("Email is required."),
-    mobile: Yup.string()
-      .matches(
-        /^(010|011|012|015)\d{8}$/,
-        "Please enter a valid Egyptian phone number (e.g., 01012345678)."
-      )
-      .required("Mobile number is required."),
+    fullName: stringRequiredWithPattern(
+      nameRegex,
+      "Name must be in English and cannot contain special characters.",
+      "Full Name is required."
+    ),
+    personalPhoto: Yup.mixed().required("personal Photo is required"),
+
+    email: stringRequiredWithPattern(
+      emailRegex,
+      "Please enter a valid email address.",
+      "Email is required."
+    ),
+
+    mobile: stringRequiredWithPattern(
+      mobileRegex,
+      "Please enter a valid Egyptian phone number (e.g., 01012345678).",
+      "Mobile number is required."
+    ),
+
     linkedin: Yup.string().url().optional(),
-    cityOfBirth: Yup.string().required("City of birth is required."),
+
+    cityOfBirth: stringRequiredWithPattern(
+      cityRegex,
+      "City of birth must be in English,cannot contain special characters.",
+      "City of birth is required."
+    ),
   }),
 
   Yup.object().shape({
-    faculty: Yup.string().required("Faculty is required."),
-    university: Yup.string().required("University is required."),
+    faculty: stringRequiredWithPattern(
+      facultyUniversityRegex,
+      "Faculty must be in English and cannot contain special characters.",
+      "Faculty is required."
+    ),
+
+    university: stringRequiredWithPattern(
+      facultyUniversityRegex,
+      "University must be in English and cannot contain special characters.",
+      "University is required."
+    ),
+
     trackName: Yup.string().required("Track name is required."),
     branch: Yup.string().required("Branch is required."),
-    program: Yup.string()
 
+    program: Yup.string()
       .oneOf(
         [
           "Professional Training Program - (9 Months)",
@@ -35,6 +83,7 @@ const stepValidationSchemas = [
         "Program must be either Professional Training Program - (9 Months) or Intensive Code Camp - (4 Months)."
       )
       .required("Program is required."),
+
     itiGraduationYear: Yup.number()
       .required("ITI Graduation Year is required.")
       .min(2000, "Graduation year must be after 2000.")
@@ -42,7 +91,36 @@ const stepValidationSchemas = [
         new Date().getFullYear(),
         `Graduation year must be less than or equal to ${new Date().getFullYear()}.`
       ),
-    intake: Yup.string().required("Intake is required."),
+
+    intake: Yup.number().test(
+      "intake-condition",
+      "The intake value must be greater than zero for the 9 Months program.",
+      function (value) {
+        const { program } = this.parent;
+
+        if (
+          program === "Professional Training Program - (9 Months)" &&
+          (!value || value <= 0)
+        ) {
+          return false;
+        }
+
+        return true;
+      }
+    ),
+
+    round: Yup.string().test(
+      "round-required-for-4-months",
+      "Round is required for 4 Months program.",
+      function (value) {
+        const { program } = this.parent;
+
+        if (program === "Intensive Code Camp - (4 Months)" && !value) {
+          return false;
+        }
+        return true;
+      }
+    ),
   }),
 
   Yup.object().shape({
@@ -52,11 +130,22 @@ const stepValidationSchemas = [
         1,
         "You have to include at least one branch you're interested to teach in!"
       )
-      .required("Preferred teaching branches are required"),
-    preferredCoursesToTeach: Yup.string().optional(),
+      .required("Preferred teaching branches are required."),
+
+    preferredCoursesToTeach: Yup.array()
+      .test(
+        "is-valid-course",
+        "Preferred courses must be in English and can include underscores and dashes.",
+        (value) => {
+          if (!value || value.length === 0) return true;
+          return value.every((course) => /^[a-zA-Z-_ ]*$/.test(course));
+        }
+      )
+      .optional(),
+
     interestedInTeaching: Yup.string()
       .oneOf(
-        ["Business sessions", "Courses"],
+        ["Business sessions", "Courses", "Both"],
         "Please specify a valid teaching interest (Business sessions or Courses)."
       )
       .required("Teaching interest is required."),
@@ -65,43 +154,19 @@ const stepValidationSchemas = [
   Yup.object().shape({
     isEmployed: Yup.boolean().required("Employment status is required."),
 
-    fullJobTitle: Yup.string().test(
-      "is-employed",
-      "Job title is required if you are employed.",
-      function (value) {
-        const { isEmployed } = this.parent;
-        return isEmployed ? !!value : true;
-      }
-    ),
+    fullJobTitle: createEmploymentTest("Job title"),
+    companyName: createEmploymentTest("Company name"),
 
-    companyName: Yup.string().test(
-      "is-employed",
-      "Company name is required .",
-      function (value) {
-        const { isEmployed } = this.parent;
-        return isEmployed ? !!value : true;
-      }
-    ),
     yearsOfExperience: Yup.number().test(
       "is-employed",
       "Years of experience is required if you are employed.",
       function (value) {
-        const { isEmployed } = this.parent;
-        if (isEmployed) {
-          if (value === undefined || value === null) {
-            return this.createError({
-              message: "Years of experience is required.",
-            });
-          }
-
-          return (
-            value > 0 ||
-            this.createError({
-              message: "Years of experience must be greater than 0.",
-            })
-          );
-        }
-        return true;
+        return this.parent.isEmployed
+          ? value > 0 ||
+              this.createError({
+                message: "Years of experience must be greater than 0.",
+              })
+          : true;
       }
     ),
 
@@ -111,10 +176,9 @@ const stepValidationSchemas = [
 
     freelancingIncome: Yup.string().test(
       "has-freelance",
-      "Freelance gain is required ",
+      "Freelance gain is required .",
       function (value) {
-        const { hasFreelanceExperience } = this.parent;
-        return hasFreelanceExperience ? !!value : true;
+        return this.parent.hasFreelanceExperience ? !!value : true;
       }
     ),
   }),
