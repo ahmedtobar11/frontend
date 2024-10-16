b9d5ae38aca18e9a8780f627a1aebfd44fb2107d
safaasamir <safaasamirsherif@gmail.com>
2024-10-16 19:21:27 +0300

    fix: refactor input rendering into a separate function

--- src/pages/MultiStepForm.jsx
+++ src/pages/MultiStepForm.jsx
@@ -45,22 +45,16 @@ const MultiStepForm = () => {
       return {};
     } catch (err) {
       const errors = {};
-      if (err.inner) {
-        err.inner.forEach((error) => {
-          errors[error.path] = error.message;
-        });
-      }
+
+      err.inner.forEach((error) => {
+        errors[error.path] = error.message;
+      });
 
       return errors;
     }
   };
 
-  const handleNext = async (
-    setCurrentStep,
-    currentStep,
-    formData,
-    setFormErrors
-  ) => {
+  const handleNext = async () => {
     const errors = await validateStep(currentStep, formData);
 
     if (Object.keys(errors).length > 0) {
@@ -72,7 +66,7 @@ const MultiStepForm = () => {
     setCurrentStep((prev) => Math.min(prev + 1, steps.length - 1));
   };
 
-  const handlePrevious = (setCurrentStep) => {
+  const handlePrevious = () => {
     setCurrentStep((prev) => Math.max(prev - 1, 0));
   };
 
@@ -104,7 +98,7 @@ const MultiStepForm = () => {
     }));
   };
 
-  const handleSubmit = async (currentStep, formData, setFormErrors) => {
+  const handleSubmit = async () => {
     setIsSubmitting(true);
     let loadingSwal;
 
@@ -130,7 +124,6 @@ const MultiStepForm = () => {
           formData
         );
 
-        loadingSwal.close();
         Swal.fire({
           title: "Thanks For Submit!",
           text: "Your Registration Done Successfully. Please wait for our answer!",
@@ -142,17 +135,17 @@ const MultiStepForm = () => {
         console.log(response);
       }
     } catch (error) {
-      if (loadingSwal) {
-        loadingSwal.close();
-      }
-
       Swal.fire({
         title: "Error!",
         text: `There was an issue: ${error.message}. Please try again.`,
         icon: "error",
       });
+
       console.log(error.message || "Error Submitting register form");
     } finally {
+      if (loadingSwal) {
+        loadingSwal.close();
+      }
       setIsSubmitting(false);
     }
   };
@@ -188,16 +181,14 @@ const MultiStepForm = () => {
 
           {currentStep < steps.length - 1 ? (
             <Button
-              onClick={() =>
-                handleNext(setCurrentStep, currentStep, formData, setFormErrors)
-              }
+              onClick={handleNext}
               text={"NEXT"}
               className="place-self-end w-32 "
               disabled={isSubmitting}
             />
           ) : (
             <Button
-              onClick={() => handleSubmit(currentStep, formData, setFormErrors)}
+              onClick={handleSubmit}
               text={"Submit"}
               className="place-self-end w-32 "
               disabled={isSubmitting}
