2f8eea040d59c4e2cd51dd6a9cb74480f027dd06
ahmedtobar11 <ahmedtobar171@gmail.com>
2024-10-21 10:15:31 +0300

    Update axiosInstance.js

--- src/services/interceptor/axiosInstance.js
+++ src/services/interceptor/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupInterceptors } from "./interceptor";
 
 const apiInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "http://98.84.176.210:3000/api/v1/",
   headers: {
     "Content-Type": "multipart/form-data",
   },
